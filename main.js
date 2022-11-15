6e1580b41ac16a3682abeb8b4fb67b60484125c3
lisamarieandersson <lisamarie.andersson@gmail.com>
2022-11-15 09:41:43 +0100

    add first files

---
+++ main.js
@@ -0,0 +1,11 @@
+window.addEventListener("DOMContentLoaded", main);
+
+function main() {
+    //Start på programmet 
+
+    setTimeout(changeBackgroundColor, 4000);
+}
+
+function changeBackgroundColor() {
+    document.body.style.background = "lightblue"
+}
