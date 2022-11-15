ab93ae9e115e1e09b84f9ed5cfc52ecd8fe0b52e
lisamarieandersson <lisamarie.andersson@gmail.com>
2022-11-15 11:03:56 +0100

    changes to css

--- main.js
+++ main.js
@@ -1,11 +1,48 @@
 window.addEventListener("DOMContentLoaded", main);
 
+let timeoutRef;
+
 function main() {
     //Start på programmet 
-
-    setTimeout(changeBackgroundColor, 4000);
+    timeoutRef = setTimeout(changeBackgroundColor, 2000);
+    setupEventListeners();
 }
 
 function changeBackgroundColor() {
-    document.body.style.background = "lightblue"
+    document.body.style.background = getRandomColor();
 }
+
+function setupEventListeners() {
+   const cancelTimeoutButton = document.getElementById("cancelTimeoutButton");
+   cancelTimeoutButton.addEventListener("click", startBackgroundChangeInterval)
+
+   const startIntervalButton = document.getElementById("startTimeoutButton");
+   startIntervalButton.addEventListener("click", startBackgroundChangeInterval)
+}
+
+function cancelBackgroundChange() {
+    clearTimeout(timeoutRef);
+}
+
+function startBackgroundChangeInterval() {
+    setInterval(changeBackgroundColor, 1000);
+}
+
+function getRandomColor() {
+    const value = Math.random();
+
+    if (value <0.2) {
+        return "blue";
+    }   else if (value < 0.4) {
+        return "red";
+    }   else if (value < 0.6) {
+        return "green";
+    }   else if (value < 0.8) {
+        return "grey";
+    }   else {
+        return "pink";
+    }
+}
+
+
+
